b5e7e756c744dcecd6d2bf6b34dec1fe9cdc2cec
nightxstudio <nightxstudio15@gmail.com>
2025-04-26 19:49:55 +0530

    Delete src directory

--- src/components/layout/MobileNavigation.tsx
+++
@@ -1,6 +0,0 @@
-
-// This component is now unused, as navigation is handled by the hamburger menu in the header.
-// It is kept for reference or removal. To fully remove: delete this file and references.
-export default function MobileNavigation() {
-  return null;
-}
--- src/pages/Index.tsx
+++
@@ -1,8 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-// Redirect to landing page
-const Index = () => {
-  return <Navigate to="/" replace />;
-};
-
-export default Index;
